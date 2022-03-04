961cf7b94a834b99e542edb6199a331b2a650944
Valeria <vcerpasalas@gmail.com>
2022-03-04 18:36:16 -0500

    Actualización .js del cambio de color del header.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 "use-strict";
 const documentReady = () => {
+  // Start header
+  const headerScroll = () => {
+    const header = document.getElementById("header");
+    const headerNavMenuIcon = document.querySelector(".header-nav__menu-icon");
+    header.classList.toggle("header--scroll", window.scrollY > 0);
+    headerNavMenuIcon.classList.toggle("header-nav__menu-icon--scroll", window.scrollY > 0);
+
+    const headerNavLink = [...document.querySelectorAll(".header-nav__link")];
+    headerNavLink.map((element) => {
+      element.classList.toggle("header-nav__link--scroll", window.scrollY > 0);
+    });
+  };
+  window.addEventListener("scroll", headerScroll);
+
+  const toggleMenu = () => {
+    const headerNavLinkList = document.getElementById("headerNavLinkList");
+    headerNavLinkList.classList.toggle("header-nav__link-list--left-0");
+  };
+  const headerNavMenu = document.getElementById("headerNavMenu");
+  headerNavMenu.addEventListener("click", toggleMenu);
+
+  const headerNavLinkItems = [...document.querySelectorAll(".header-nav__link-item")];
+  headerNavLinkItems.map((element) => {
+    element.addEventListener("click", toggleMenu);
+  });
+  // End header
+
   const tabHeaderItems = document.querySelectorAll(".tabs__tab-item");
   const tabContentItems = document.querySelectorAll(".tab-content__item");
   const tabHeaderIcons = document.querySelectorAll(".tab__icon");
