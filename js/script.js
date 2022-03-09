694776cfe13c959cff40ce6ae891a70a595ac176
Diego <diegojairhl@gmail.com>
2022-03-08 19:09:30 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -72,12 +72,6 @@ const documentReady = () => {
     item.addEventListener("mouseenter", enterTabItem);
     item.addEventListener("mouseleave", leaveTabItem);
   });
-
-  const displayProximamente = () => {
-    document.getElementById("demo").innerHTML = "Próximamente...";
-  };
-
-  metricasNextYearbtn.addEventListener("click", displayProximamente);
 };
 
 document.addEventListener("DOMContentLoaded", documentReady);
